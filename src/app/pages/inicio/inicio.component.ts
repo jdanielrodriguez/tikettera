716e921aea9e6f3d9f3ce1db8692b360277d1fb2
jdanielrodriguez <daniel.rodriguez@vidoomy.com>
2022-11-27 16:24:15 -0600

    added pages first version

---
+++ src/app/pages/inicio/inicio.component.ts
@@ -0,0 +1,162 @@
+import { Component, OnInit } from '@angular/core';
+import { NotificationsService } from 'angular2-notifications';
+import { BlockUI, NgBlockUI } from 'ng-block-ui';
+import { Proveedor, ListaBusqueda } from './../../interfaces';
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.component.html',
+  styleUrls: []
+})
+export class InicioComponent implements OnInit {
+  constructor(
+    private _service: NotificationsService,
+  ) { }
+  set numReg(value: number) {
+    this._numReg = value;
+  }
+  get numReg(): number {
+    return this._numReg;
+  }
+  set limit(value: number) {
+    this._limit = value;
+  }
+  get limit(): number {
+    return this._limit;
+  }
+  set offset(value: number) {
+    this._offset = value;
+  }
+  get offset(): number {
+    const actual = (this.page - 1) * this._limit;
+    this._offset = actual;
+    return this._offset;
+  }
+  set page(value: number) {
+    this._page = value;
+  }
+  get page(): number {
+    return this._page;
+  }
+  set proveedoresLista(value: ListaBusqueda[]) {
+    this._proveedoresLista = value;
+  }
+  get proveedoresLista(): ListaBusqueda[] {
+    return this._proveedoresLista;
+  }
+  get proveedoresListaAuxiliar(): ListaBusqueda[] {
+    return this._proveedoresLista;
+  }
+  set proveedor(value: ListaBusqueda) {
+    this._proveedor = value;
+  }
+  get proveedor(): ListaBusqueda {
+    return this._proveedor;
+  }
+  @BlockUI() blockUI!: NgBlockUI;
+  private _proveedor: ListaBusqueda = new ListaBusqueda();
+  private _numReg = 0;
+  private _limit = 10;
+  private _offset = 0;
+  private _page = 1;
+  active = 1;
+  sliders = [{ url: 'https://placehold.it/500x250?text=Cargando...', titulo: '', descripcion: '' }];
+  private _proveedoresLista: ListaBusqueda[] = [
+    {
+      id: 1,
+      nombre: 'Cargando...',
+      imagen: 'https://placehold.it/500x250?text=Cargando...',
+    },
+    {
+      id: 1,
+      nombre: 'Cargando...',
+      imagen: 'https://placehold.it/500x250?text=Cargando...',
+    },
+    {
+      id: 1,
+      nombre: 'Cargando...',
+      imagen: 'https://placehold.it/500x250?text=Cargando...',
+    }, {
+      id: 1,
+      nombre: 'Cargando...',
+      imagen: 'https://placehold.it/500x250?text=Cargando...',
+    }
+  ];
+  private _proveedoresListaAuxiliar: ListaBusqueda[] = this._proveedoresLista;
+  public options = {
+    position: ['bottom', 'right'],
+    timeOut: 2000,
+    lastOnBottom: false,
+    animate: 'scale',
+    showProgressBar: false,
+    pauseOnHover: true,
+    clickToClose: true,
+    maxLength: 200
+  };
+
+  ngOnInit(): void {
+  }
+
+  cargarProveedor(value: ListaBusqueda) {
+    if (value.id) {
+      // this._proveedor = value;
+      // this._proveedoresLista = this._proveedoresLista.filter((v: ListaBusqueda) =>
+      //   v.nombre.toLowerCase().indexOf(value.nombre.toLowerCase()) > -1);
+    } else {
+      this._proveedor = new ListaBusqueda();
+      this._proveedoresLista = this._proveedoresListaAuxiliar;
+    }
+  }
+
+  cargarProveedores() {
+    this.blockUI.start();
+    const data = {
+      id: 0,
+      estado: 0,
+      filter: 'proveedor&limit=' + this.limit + '&offset=' + this.offset + '&estado=1'
+    };
+    // this.proveedorService.getAll(data)
+    //   .then((response: { status: number, numReg: number, objeto: Proveedor[] }) => {
+    //     this._numReg = response.numReg;
+    //     this._proveedoresLista.length = 0;
+    //     this._proveedoresListaAuxiliar.length = 0;
+    //     try {
+    //       response.objeto.forEach((element: Proveedor) => {
+    //         const datas: ListaBusqueda = {
+    //           imagen: (element.imagenes && element.imagenes.length > 0) ? element.imagenes[0].url
+    //             : ((element.usuario.imagenes && element.usuario.imagenes.length > 0) ? element.usuario.imagenes[0].url
+    //               : 'https://placehold.it/250x200'),
+    //           nombre: element.nombre ? element.nombre : 'No Name',
+    //           id: element.id,
+    //           validacion: 5,
+    //           inventario: null
+    //         };
+    //         this._proveedoresLista.push(datas);
+    //       });
+    //       const prov = new ListaBusqueda();
+    //       prov.nombre = '';
+    //       this.cargarProveedor(prov);
+    //       this._proveedoresListaAuxiliar = this._proveedoresLista;
+    //       this.sliders.length = 0;
+    //     } catch (exception) {
+    //       console.log(exception);
+    //     } finally {
+    //       this.blockUI.stop();
+    //     }
+    //   })
+    //   .catch(error => {
+    //     this.blockUI.stop();
+    //     this.createError(error);
+    //   });
+  }
+
+  cambioPagina(value: any) {
+    this._page = value;
+    this.cargarProveedores();
+  }
+  createSuccess(success: string) {
+    this._service.success('¡Éxito!', success);
+  }
+  createError(error: string) {
+    this._service.error('¡Error!', error);
+  }
+}
---
+++ src/app/pages/search/busqueda.component.ts
@@ -0,0 +1,113 @@
+import { Component, OnInit, Output, ViewChild, EventEmitter, Input, ElementRef } from '@angular/core';
+import { NgbTypeahead } from '@ng-bootstrap/ng-bootstrap';
+import { BlockUI, NgBlockUI } from 'ng-block-ui';
+import { Observable, Subject, merge } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter, map } from 'rxjs/operators';
+import { ListaBusqueda } from '../../interfaces';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+  styleUrls: ['./busqueda.component.css']
+})
+export class BusquedaComponent implements OnInit {
+  @BlockUI() blockUI!: NgBlockUI;
+  private _lista: ListaBusqueda[] = [];
+  private _titulo = '';
+  private _seleccion: ListaBusqueda = new ListaBusqueda();
+  public imagenDefault = 'https://placehold.it/250x200?text=Logo';
+  private _seleccionador: EventEmitter<ListaBusqueda> = new EventEmitter<ListaBusqueda>();
+  private _seleccionado: ListaBusqueda = new ListaBusqueda();
+  constructor() { }
+  @ViewChild('instance', { static: true }) instance!: NgbTypeahead;
+  focus$ = new Subject<string>();
+  click$ = new Subject<string>();
+  ngOnInit(): void {
+    if (this._seleccionado.id) {
+      this._seleccionador.emit(this._seleccionado);
+    }
+  }
+  search = (text$: Observable<string>) => {
+    const debouncedText$ = text$.pipe(debounceTime(200), distinctUntilChanged());
+    const clicksWithClosedPopup$ = this.click$.pipe(filter(() => !(this.instance ? this.instance.isPopupOpen() : true)));
+    const inputFocus$ = this.focus$;
+
+    // return merge(debouncedText$, inputFocus$, clicksWithClosedPopup$).pipe(
+    //   map(term => (term === '' ? this._lista
+    //     : this._lista.filter((v: ListaBusqueda) => v.nombre.toLowerCase().indexOf(term.toLowerCase()) > -1)).slice(0, 10))
+    // );
+  }
+  limpiarValor(value: any, option: any) {
+    const temp: ListaBusqueda = new ListaBusqueda();
+    this._seleccionado = temp;
+    this._seleccionador.emit(this._seleccionado);
+  }
+  seleccionarValor(value: any, option: any) {
+    this.blockUI.start();
+    let temp: ListaBusqueda = new ListaBusqueda();
+    const index = this._lista.findIndex((element: ListaBusqueda) => element.id === value.id);
+    if (index >= 0) {
+      temp = this._lista[index];
+    }
+    if (temp.nombre) {
+      this._seleccionado = temp;
+    }
+    this._seleccionador.emit(this._seleccionado);
+    this.blockUI.stop();
+  }
+  resultFormatBandListValue(value: ListaBusqueda) {
+    return value.nombre;
+  }
+  inputFormatBandListValue(value: ListaBusqueda) {
+    if (value.nombre) {
+      return value.nombre;
+    } else {
+      try {
+        return this._seleccion.nombre;
+      } catch (error) {
+      }
+    }
+
+
+  }
+  @Input()
+  set lista(value: ListaBusqueda[]) {
+    this._lista = value;
+  }
+  get lista(): ListaBusqueda[] {
+    return this._lista;
+  }
+  @Input()
+  set titulo(value: string) {
+    this._titulo = value;
+  }
+  get titulo(): string {
+    return this._titulo;
+  }
+  @Input()
+  set seleccion(value: ListaBusqueda) {
+    if (value) {
+      const index = this._lista ? this._lista.findIndex(element => element.id === value.id) : -1;
+      const select: ListaBusqueda = index >= 0 ? this._lista[index] : (new ListaBusqueda());
+      if (value && value.id === null) {
+        this._seleccionado = new ListaBusqueda();
+        this._seleccion = new ListaBusqueda();
+      } else {
+        this._seleccion = select;
+        this._seleccionado = select;
+      }
+    }
+  }
+  get seleccion(): ListaBusqueda {
+    return this._seleccion;
+  }
+
+  @Output()
+  get seleccionador(): EventEmitter<ListaBusqueda> {
+    this._seleccionador.emit(this._seleccionado);
+    return this._seleccionador;
+  }
+  get seleccionado(): ListaBusqueda {
+    return this._seleccionado;
+  }
+}

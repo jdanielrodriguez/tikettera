2ddfb43b09633f662e9dcc5490968a57624107b9
jdanielrodriguez <daniel.rodriguez@vidoomy.com>
2023-01-15 23:02:05 -0600

    :hammer: added register functionality

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './guards/auth.guard';
-import { HomeGuard } from './guards/home.guard';
 
-import { LogoutComponent } from './components/logout/logout.component';
 import { ChangePassComponent } from './components/change-pass/change-pass.component';
+import { LogoutComponent } from './components/logout/logout.component';
 import { LoginComponent } from './login/login.component';
-import { InicioComponent } from './pages/inicio/inicio.component';
 import { BoletosComponent } from './pages/boletos/boletos.component';
-import { LocalidadesComponent } from './pages/localidades/localidades.component';
 import { BuscarEventoComponent } from './pages/buscar-evento/buscar-evento.component';
 import { ComprarComponent } from './pages/comprar/comprar.component';
-import { PagoComponent } from './pages/pago/pago.component';
 import { FacturaComponent } from './pages/factura/factura.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { LocalidadesComponent } from './pages/localidades/localidades.component';
 import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+import { PagoComponent } from './pages/pago/pago.component';
 import { RecoveryComponent } from './recovery/recovery.component';
 import { RegisterComponent } from './register/register.component';
 
@@ -21,7 +20,7 @@ const routes: Routes = [
   { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(e => e.DashboardModule), canActivate: [AuthGuard] },
   { path: '', component: InicioComponent },
   { path: 'nosotros', component: NosotrosComponent },
-  { path: 'chage-pass', component: ChangePassComponent },
+  { path: 'change-pass', component: ChangePassComponent },
   { path: 'recovery', component: RecoveryComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },

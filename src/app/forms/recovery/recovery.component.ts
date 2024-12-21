19209e0058320eab80a7db92402b3ea3946f3199
jdanielrodriguez <dannyjose1112@hotmail.com>
2024-12-20 21:33:27 -0600

    :pencil: added new form reactive

--- src/app/forms/recovery/recovery.component.ts
+++ src/app/forms/recovery/recovery.component.ts
@@ -1,26 +1,28 @@
 import { Component, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
 import { Menus, Perfil, Response, ResponseCAPTCHA } from "./../../interfaces";
 import { Sesion } from './../../common/sesion';
 import { AuthServices } from "./../../services/auth.service";
 import { Modal } from "./../modal.component";
-declare var $: any
+declare var $: any;
 @Component({
   selector: 'app-recovery-form',
   templateUrl: './recovery.component.html',
   styleUrls: ['./recovery.component.css']
 })
 export class RecoveryComponent implements OnInit, OnDestroy {
-  @ViewChild(Modal) recoveryModal!: Modal
+  @ViewChild(Modal) recoveryModal!: Modal;
   @Output() component: EventEmitter<string> = new EventEmitter<string>();
   @Input() componentStr: string = '';
   @Input() data: Perfil = new Perfil();
   @Input() esModal: boolean = false;
   @Input() dinamicLink: string = '';
   @Input() titulo: string = '';
+  recoveryForm!: FormGroup;
 
   constructor(
+    private fb: FormBuilder,
     private authServices: AuthServices,
     private modalService: NgbModal,
     private mySesion: Sesion,
@@ -28,18 +30,22 @@ export class RecoveryComponent implements OnInit, OnDestroy {
   ) {
     config.backdrop = 'static';
     config.keyboard = true;
-    config.size = 'lg'
+    config.size = 'lg';
   }
   ngOnInit() {
+    this.recoveryForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]]
+    });
+
     this.mySesion.scrollTop();
     if (this.esModal) {
-      let temp = Modal
+      let temp = Modal;
       if (this.titulo) {
         if (this.recoveryModal) {
           this.recoveryModal.titulo = this.titulo;
         }
       }
-      temp.prototype.titulo = this.titulo
+      temp.prototype.titulo = this.titulo;
       this.modalService.open(temp);
     }
     this.mySesion.showCaptcha();
@@ -49,21 +55,22 @@ export class RecoveryComponent implements OnInit, OnDestroy {
       evento.stopPropagation();
     }
     if (this.modalService.hasOpenModals()) {
-      this.componentStr = data.url
+      this.componentStr = data.url;
       this.component.emit(this.componentStr);
     } else {
-      this.mySesion.navegar({ url: `${data.url}` })
+      this.mySesion.navegar({ url: `${data.url}` });
     }
   }
-  async recovery(form: NgForm) {
-    let perfil: Perfil = new Perfil(form.value);
+  async onSubmit() {
+    if (this.recoveryForm.invalid) {
+      return;
+    }
+    let email = this.recoveryForm.get('email')?.value;
     let validateCaptcha = await this.mySesion.validateCaptcha('recovery');
     this.mySesion.loadingStart();
     if (!validateCaptcha) {
       this.mySesion.createError("Error validando Captcha.");
       this.mySesion.loadingStop();
-      validateCaptcha = await this.mySesion.validateCaptcha('recovery');
-      this.recovery(form);
       return;
     }
     const captchaData = {
@@ -73,27 +80,24 @@ export class RecoveryComponent implements OnInit, OnDestroy {
       .subscribe({
         next: (response: ResponseCAPTCHA) => {
           if (response.objeto.success) {
-            if (perfil.email) {
-              this.restore(perfil.email);
-            }
+            this.restore(email);
           }
         },
         error: (error: any) => {
-          this.mySesion.createError('Error iniciando sesion');
+          this.mySesion.createError('Error validando captcha.');
           this.mySesion.loadingStop();
         },
         complete: () => { authServ.unsubscribe(); }
       });
-
   }
   restore(email: string) {
     let dat: any = {
       email: btoa(email)
-    }
+    };
     const authServ = this.authServices.restore(dat)
       .subscribe({
         next: (response: Response) => {
-          if (response.status != 200) {
+          if (response.status !== 200) {
             this.mySesion.createError('No se encuentra el usuario ingresado');
             this.mySesion.loadingStop();
             return;
@@ -101,7 +105,7 @@ export class RecoveryComponent implements OnInit, OnDestroy {
           this.mySesion.hideCaptcha();
           let url = "../";
           if (this.mySesion.lastLink) {
-            url = this.mySesion.lastLink
+            url = this.mySesion.lastLink;
           }
           this.mySesion.navegar({ url });
           this.mySesion.loadingStop();
@@ -121,5 +125,4 @@ export class RecoveryComponent implements OnInit, OnDestroy {
       this.mySesion.captchaSubscription.unsubscribe();
     }
   }
-
 }
